c352e16563a163f57f0b379eb1ff5330e97d0913
Tushar Malankiya <253198629+tushar-the-dev@users.noreply.github.com>
2026-04-28 18:54:35 +0530

    connect ai

---
+++ src/core/setGreet.js
@@ -0,0 +1,19 @@
+const {GoogleGenAI} = require("@google/genai");
+
+class setGreet{
+    constructor({apiKey}){
+        this.apiKey = apiKey;
+        this.ai = new GoogleGenAI({apiKey: apiKey});
+    }
+
+    async Greet(name){
+       const res =  await this.ai.models.generateContent({
+        model: 'gemini-3-flash-preview',
+        contents: `My name is ${name}. only greet me. don't ask any questions`
+    });
+
+    return res.text;
+    }
+}
+
+module.exports = setGreet;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const greet = require('./greet');
 const farewell = require('./farewell');
 const formatName = require('./utils/formatName');
+const setGreet = require('./core/setGreet');
 
 module.exports = {
   greet,
   farewell,
-  formatName
+  formatName,
+  setGreet
 };
